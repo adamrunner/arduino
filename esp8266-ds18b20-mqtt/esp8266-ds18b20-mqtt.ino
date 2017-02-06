fb0a8d7c311fb4aff108d41b815f293de734208e
Adam Runner <adamrunner@gmail.com>
2017-02-06 10:29:14 -0800

    update esp8266 plant environment controller script

--- esp8266-ds18b20-mqtt/esp8266-ds18b20-mqtt.ino
+++ esp8266-ds18b20-mqtt/esp8266-ds18b20-mqtt.ino
@@ -1,20 +1,34 @@
 /*
   MQTT Temperature Client
+  Hardware Setup:
+  ESP8266 running from 5V standby of XBOX PSU
   DS18B20 Connected on Pin 2 / D4
+  XBOX PS_ON Connected on D5 - set to "HIGH" to turn on 12V supply
+  Water Pump controlled by MOSFET on D7
+  Water Pump power comes from +5VSB
+  MOSFET for LED on D6
+
   OTA Uploads - default port of 8266
   TEMP_SERVER - server IP
   MQTT_PORT   - default of 1883
 
   Reads temperature every 5m and posts it to the "outTopic" topic on the MQTT Server
+    - message has this format HOSTNAME,TEMP_F (e.g. ESP_299021,36.84)
+    - HOSTNAME is used to identify each sensor uniquely
+    - HOSTNAME is based off of the MAC addr of each sensor
+  Subscribes to the PUMP topic
+    - a 1 written to the PUMP topic will turn on the water pump
+    - program monitors the amount of time that pump has been running
+    - shuts pump off after RUN_PUMP_LENGTH (in ms) has elapsed
   Subscribes to the LED topic
     - casts values to int
-    - writes the int to an analog signal on D5
+    - writes the int to an analog signal on LED Pin (D6), values are 0-1024
   Subscribes to the PSU topic
     - 0 turns off PSU
     - 1 turns on PSU
   Subscribes to the TEMP_REQ topic
     - when a 1 is received
-    - it sends an immediate from the temperature sensor
+    - it sends an immediate update from the temperature sensor
 
   TODO: Give the topics better names - inTopic / outTopic are awful
 */
@@ -27,8 +41,12 @@
 #include <WifiCreds.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS D4
 #define MQTT_PORT 1883
+#define PUMP D7
+#define PSU D6
+#define LED D5
+#define RUN_PUMP_LENGTH 5000 // amount of time to let the pump run in seconds
 uint8_t MAC_array[6];
 char MAC_char[18];
 
@@ -41,9 +59,11 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 long lastMsg = 0;
+float temp   = 0.0;
 char msg[50];
-float temp = 0.0;
 char currentHostname[14];
+unsigned long pumpStartTime;
+bool pumpIsRunning;
 
 
 float getTemp(){
@@ -57,8 +77,9 @@ float getTemp(){
 
 
 void setup() {
-  pinMode(D5, OUTPUT);     // Initialize the D5 pin as an output
-  pinMode(D6, OUTPUT);     // Initialize the D6 pin as an output
+  pinMode(LED, OUTPUT);     // Initialize the LED pin (D6) as an output
+  pinMode(PSU, OUTPUT);     // Initialize the PSU pin (D5) as an output
+  pinMode(PUMP, OUTPUT);     // D7 pin - Pump
   Serial.begin(115200);
   setup_wifi();
   // Setup OTA Uploads
@@ -124,11 +145,8 @@ void callback(char* topic, byte* payload, unsigned int length) {
   payload[length] = '\0';
   char *payloadS = (char *) payload;
 
+  // TODO: Clean up logic - possibly by using a case statement?
 
-  // TODO: Handle different topics - case statement?
-  // if topic == LED
-  // if topic == PSU
-  // else
   // Switch on the LED if an 1 was received as first character
   if (strcmp(topic, "TEMP_REQ") == 0){
     if ((char)payload[0] == '1') {
@@ -136,19 +154,35 @@ void callback(char* topic, byte* payload, unsigned int length) {
       sendTempUpdate();
     }
   }
+
+  if(strcmp(topic, "PUMP") == 0){
+    if(pumpIsRunning == false && (char)payload[0] == '1'){
+      startPump();
+    }
+    if((char)payload[0] == '0'){
+      stopPump();
+    }
+  }
+
   if (strcmp(topic, "LED") == 0 ){
+    char ledMessage[12]= "LED: ";
+    strcat(ledMessage, payloadS);
     unsigned int ledValue = atoi(payloadS);
+    client.publish("outTopic", ledMessage);
     Serial.print("Setting LED to: ");
     Serial.println((int) ledValue);
-    analogWrite(D5, (int) ledValue);
+    analogWrite(LED, (int) ledValue);
   }
+
   if( strcmp(topic, "PSU") == 0){
     if ((char)payload[0] == '1') {
       Serial.print("Turning power supply ON!");
-      digitalWrite(D6, HIGH);
+      client.publish("outTopic", "PSU ON");
+      digitalWrite(PSU, HIGH);
     }else{
       Serial.print("Turning power supply OFF!");
-      digitalWrite(D6, LOW);
+      client.publish("outTopic", "PSU OFF");
+      digitalWrite(PSU, LOW);
     }
   }
 
@@ -165,6 +199,7 @@ void reconnect() {
       client.subscribe("TEMP_REQ");
       client.subscribe("LED");
       client.subscribe("PSU");
+      client.subscribe("PUMP");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -177,13 +212,29 @@ void reconnect() {
 void sendTempUpdate(){
   getTemp();
   char tempChar[7];
+  // Convert float to char array
   // dtostrf(FLOAT,WIDTH,PRECSISION,BUFFER);
   dtostrf(temp,4,2,tempChar);
+  // Format msg
+  // TODO: move msg to a local variable? Unneeded outside of this scope.
   sprintf(msg, "%s,%s", currentHostname, tempChar);
 
   Serial.print("Publish message: ");
   Serial.println(msg);
   client.publish("outTopic", msg);
+}
+
+void startPump() {
+  pumpIsRunning = true;
+  pumpStartTime = millis();
+  client.publish("outTopic", "Starting Pump");
+  digitalWrite(PUMP, HIGH);
+}
+
+void stopPump() {
+  pumpIsRunning = false;
+  client.publish("outTopic", "Stopping Pump");
+  digitalWrite(PUMP, LOW);
 }
 
 void loop() {
@@ -194,6 +245,9 @@ void loop() {
   client.loop();
 
   long now = millis();
+  if (now - pumpStartTime > RUN_PUMP_LENGTH && pumpIsRunning){
+    stopPump();
+  }
   if (now - lastMsg > SEND_TEMP_INTERVAL) {
     lastMsg = now;
     sendTempUpdate();
